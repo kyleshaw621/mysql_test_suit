6ace7e704fee175a54203bf1d7dd7997f8a76c9f
kyleShaw621 <xtk621@gmail.com>
2013-07-10 10:20:45 +0800

    new io.sh

--- sysbench/fileio/io.sh
+++ sysbench/fileio/io.sh
@@ -1,40 +1,53 @@
 #!/bin/sh
+logfile=io.log
+filesize=500M
+testmode=rndrw
 
-if [ $# -ne "1" ];then
-	echo "$0 [prepare | run | clean | auto ]"
-	exit 1
-fi
+Usage() {
+	cat <<EOF
+	Usage : $0 [-p] [-r] [-c] [-a] [-k] [-h]
+		p : prepare files
+		r : run test
+		c : clean files
+		a : auto run prepare, tset and clean
+		k : analyse last output
+		h : display help message.
+		m : test mode. can select seqwr[顺序写], seqrewr[顺序重写], seqrd[顺序读], rndrd[顺序重读], rndwr[随机写], rndrw[随机读写](default). 
+EOF
+}
 
 _prepare() {
-	sysbench --test=fileio --file-total-size=500M prepare
+	echo "prepare files size is $filesize"
+	sysbench --test=fileio --file-total-size=$filesize prepare
 }
 
 _run() {
-	sysbench --test=fileio --file-total-size=500M --file-test-mode=rndwr --init-rng=on --max-time=30 --max-requests=0 run 
+	echo "test type is $testmode. filesize is $filesize"
+	sysbench --test=fileio --file-total-size=$filesize --file-test-mode=$testmode --init-rng=on --max-time=30 --max-requests=0 run > $logfile
 }
 
 _clean() {
-	sysbench --test=fileio --file-total-size=500M cleanup 
+	sysbench --test=fileio --file-total-size=$filesize cleanup 
 }
 
-options=$1
-case $options in
-	prepare)
-		echo "prepare"
-		_prepare;;
-	run)
-		echo "run"
-		_run;;
-	clean)
-		echo "clean"
-		_clean;;
-	auto)
-		echo "auto"
-		_prepare
-		_run
-		_clean;;
-	*)
-		echo "unknown options"
-		exit;;
-esac
-	
+analyse() {
+	Throughput=$(cat $logfile | grep "Total trans" | awk '{print $NF}')
+	request=$(cat $logfile | grep "Requests/sec"| awk '{print $1}')
+	echo "吞吐量: $Throughput"
+	echo "请求量: $request Requests/sec"
+}
+
+while getopts prcahk OPTION; do
+	case "$OPTION" in 
+		p) _prepare ;;
+		r) _run ;;
+		c) _clean ;;
+		a) _prepare
+		   _run
+		   _clean ;;
+		h) Usage;;
+		k) analyse
+		   exit 0;;
+		*) Usage;;
+	esac
+done
